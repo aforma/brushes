16cfa4edb03fd75a31c6e49e0713ec95f10a9aac
Giulian Drimba <giuliandrimba@gmail.com>
2020-05-02 17:35:24 +0200

    Create line

--- src/brush/line.js
+++ src/brush/line.js
@@ -6,26 +6,30 @@ module.exports = (_ctx, opts) => {
   }
   const config = {
     lineWidth: options.lineWidth || 10,
-    strokeStyle: options.strokeStyle || "black"
+    strokeStyle: options.strokeStyle || "black",
   };
 
   return {
-    draw(x, y) {
-      if (!this.lastPoint) {
-        this.lastPoint = { x, y };
-      } else if (!x && !y) {
-        this.lastPoint = undefined;
-      }
+    draw(lines) {
       ctx.strokeStyle = config.strokeStyle;
       ctx.lineWidth = config.lineWidth;
-      this.drawLine(this.lastPoint.x, this.lastPoint.y, x, y);
-      this.lastPoint = { x, y };
+      for (let i = 0; i < lines.length; i += 1) {
+        for (let j = 1; j < lines[i].length; j++) {
+          const points = lines[i];
+          this.drawLine(
+            points[j - 1].x,
+            points[j - 1].y,
+            points[j].x,
+            points[j].y
+          );
+        }
+      }
     },
     drawLine(fromX, fromY, x, y) {
       ctx.beginPath();
       ctx.moveTo(fromX, fromY);
       ctx.lineTo(x, y);
       ctx.stroke();
-    }
+    },
   };
 };
--- src/painter/mouse.js
+++ src/painter/mouse.js
@@ -1,35 +1,35 @@
 module.exports = function(drawer, canvas) {
   const mouse = { x: 0, y: 0 };
-  let points = [];
+  let lines = [];
   const bounds = canvas.getBoundingClientRect();
   const scale =
     parseInt(canvas.getAttribute("width")) / parseInt(canvas.style.width);
   let mouseIsDown = false;
 
   document.onmousedown = e => {
-    points = [];
     mouse.x = (e.pageX - bounds.x) * scale;
     mouse.y = (e.pageY - bounds.y) * scale;
     mouseIsDown = true;
+    lines.push([])
   };
 
   document.onmousemove = e => {
     if (mouseIsDown) {
       mouse.x = (e.pageX - bounds.x) * scale;
       mouse.y = (e.pageY - bounds.y) * scale;
+      lines[lines.length - 1].push({ x: mouse.x, y: mouse.y });
     }
   };
 
   document.onmouseup = e => {
     mouseIsDown = false;
-    drawer();
   };
 
   return {
     done() {},
     draw() {
       if (mouseIsDown) {
-        drawer(mouse.x, mouse.y);
+        drawer(lines);
       }
     }
   };
